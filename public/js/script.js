1438c6248d83b7d505bed10ec86253316021cde1
HP-Komputer\HP <mateuszn11@gmail.com>
2019-02-12 07:59:23 +0100

    poprawki dalej

--- public/js/script.js
+++ public/js/script.js
@@ -54,7 +54,6 @@ function deleteUser(id) {
 
 function adminStart()
 {
-    getAdvertsAdmin();
     getUsers();
 }
 
--- views/DefaultController/index.php
+++ views/DefaultController/index.php
@@ -12,9 +12,7 @@
         <div class="col-3">
         </div>
         <div class="col-6 motto">
-            <h1>Znajdz odpowiednia osobe lub rzecz.</h1>
             <a href="?page=add_advert"><button onclick="" class="button-primary">Dodaj ogłoszenie</button></a>
-            <h1>Szukasz czegos dla siebie?</h1>
             <a href="?page=search&category=building"><button onclick="" class="button-primary">Przeglądaj oferty</button></a>
         </div>
         <div class="col-3">
@@ -26,43 +24,43 @@
 <div class="container main">
     <div class="row">
         <div class="col-12" style="float:left;">
-            <div class="icon">
+            <div class="row icon">
                 <p>Budownictwo</p>
                 <a href="?page=search&category=building"><i class="fas fa-building" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Transport</p>
                 <a href="?page=search&category=transport"><i class="fas fa-car" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Programowanie</p>
                 <a href="?page=search&category=programming"><i class="fas fa-code" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Korepetycje</p>
                 <a href="?page=search&category=education"><i class="fas fa-chalkboard" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Muzyka</p>
                 <a href="?page=search&category=music"><i class="fas fa-music" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Pomoc domowa</p>
                 <a href="?page=search&category=home"><i class="fas fa-home" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Fotografia</p>
                 <a href="?page=search&category=photo"><i class="fas fa-camera" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Prawo</p>
                 <a href="?page=search&category=law"><i class="fas fa-gavel" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Księgowość</p>
                 <a href="?page=search&category=accounting"><i class="fas fa-calculator" style="font-size: 30px;"></i></a>
             </div>
-            <div class="icon">
+            <div class="row icon">
                 <p>Marketing</p>
                 <a href="?page=search&category=marketing"><i class="fas fa-comments-dollar" style="font-size: 30px;"></i></a>
             </div>
